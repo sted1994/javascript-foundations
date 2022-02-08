68a599bb70b967425786de2ef56f8b39d1a59067
Stephen Tedesco <s_tylert@icloud.com>
2022-02-07 18:23:32 -0700

    Inital Commit

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,6 +2,19 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+    if (color === undefined){
+      this.color = 'white';
+    } else {
+      this.color = color
+    }
+  }
+  isWhite(){
+    if (this.color !== "white"){
+      return false;
+    }
+  }
+  says(sparklyWord) {
+    return `**;* ${sparklyWord} *;**`
   }
 }
 
