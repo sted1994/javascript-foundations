1ed64472eb5599361fd8bd28b1d0f9c29e2912a1
Stephen Tedesco <s_tylert@icloud.com>
2022-02-07 19:03:11 -0700

    Initial Commit

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name,
+    this.rider = rider,
+    this.hungry = true
+    this.timesAte = 0
+  }
+  greet(){
+    return `Hi, ${this.rider}!`
+  }
+  eat(){
+    var timesAte = 0;
+      this.timesAte++
+      if(this.timesAte === 3){
+        this.hungry = false
+      }
+    }
 
+  }
+
+
+module.exports = Dragon
